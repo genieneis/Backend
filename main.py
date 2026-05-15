fce3cea89bcc6c88afd471da476fbab19fa0a771
mskwon <mskwon0601@gmail.com>
2026-05-15 16:29:16 +0900

    chore: stt관련 라우터 off

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI
 
 load_dotenv()
 
-from api.stt import router as stt_router
+# from api.stt import router as stt_router
 from api.neis import router as neis_router
 
 app = FastAPI()
 
-app.include_router(stt_router)
+# app.include_router(stt_router)
 app.include_router(neis_router)
 
 
