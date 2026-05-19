d8214a03ed1478f77e9d5aa6dbdddc0a243b5b31
mskwon <mskwon0601@gmail.com>
2026-05-19 16:03:00 +0900

    chore: stt 관련 이전 코드 제거

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
-# from api.stt import router as stt_router
 from api.neis import router as neis_router
 
 app = FastAPI()
 
-# app.include_router(stt_router)
 app.include_router(neis_router)
 
-# 임시 CORS 설정 - 실제 배포 시에는 배포된 프론트엔드 도메인으로 변경 필요
+# 임시 CORS 설정 - 실제 배포 시에는 배포된 프론트엔드 도메인만 허용으로 변경해야함
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
