04ff0ba5f0c88c9386fffe66e20ef83f21471e91
mskwon <mskwon0601@gmail.com>
2026-05-25 11:41:19 +0900

    feat: 수업 요약 API 엔드포인트 추가

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ load_dotenv()
 
 from api.neis import router as neis_router
 from api.stt import router as stt_router
+from api.summary import router as summary_router
 
 app = FastAPI()
 
 app.include_router(neis_router)
 app.include_router(stt_router)
+app.include_router(summary_router)
 
 # 임시 CORS 설정 - 실제 배포 시에는 배포된 프론트엔드 도메인만 허용으로 변경해야함
 app.add_middleware(
